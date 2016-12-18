903d88be68ce506b9335ef9446d54fa1421ff4f9
Amanda Jopp <aclaire488@gmail.com>
2016-12-18 13:25:04 -0600

    button working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,29 @@
-console.log('sourced');
-
 $(document).ready(function(){
   console.log('meow');
+
+//button to add new task and then run newTask function
+$('#addTask').on('click', function(){
+  console.log('clicked');
+  newTask();
+  $('#task').val(''); //empties input field after click
+});
+
+//newTask function creates object
+var newTask = function(){
+  objectToSend = {
+    task: $('#task').val()
+  };
+  console.log(objectToSend);
+};
+
+$.ajax({
+  type:'POST',
+  url:'/addTask',
+  data: objectToSend,
+  success: function(response){
+    console.log('Posted: ', response);
+  }
+});
+});
+
 }); //end document ready
